3eea7f721a1b8a8d99df5665c37bc16bb7f57b8b
PosokhovVadim <posokhovvadyusha@gmail.com>
2022-09-29 22:45:32 +0400

    first commit

---
+++ Graph.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace Graph
+{
+    class Graph
+    {
+        #region fields
+
+        private bool IsWeighted { get; set; } // взвешанный или нет
+        private bool IsOriented { get; set; } //ориентированный или нет
+
+        public bool _isweighted;
+        public virtual bool isweighted { 
+            get { return _isweighted; }
+            set { _isweighted = value; }
+        }
+        private string Path;
+        private bool fl;
+        //Vertex V = new Vertex();
+        
+        //List<Edge> E = new List<Edge>();
+
+
+        private Dictionary<Vertex, List<Dictionary<Vertex, Weight>>> G = new Dictionary<Vertex, List<Dictionary<Vertex, Weight>>>();
+        private List<Edge> Edges = new List<Edge>();
+        // private List<Vertex> Vvv = new List<Vertex>();
+
+        private List<Dictionary<Vertex, Weight>> V_list = new List<Dictionary<Vertex, Weight>>();
+        private Dictionary<Vertex, Weight> tmp_value = new Dictionary<Vertex, Weight>();
+
+        Vertex x = new Vertex();
+        Weight w = new Weight();
+
+        
+        #endregion
+
+        #region constructors
+        public Graph() { }
+
+        
+        public Graph(string path) {
+            Path = path;
+            using (FileStream Source = new FileStream(path, FileMode.Open))
+            {
+                using (StreamReader read = new StreamReader(Source))
+                {
+                    string s = read.ReadToEnd();
+                    string[] line = s.Split("\n");
+
+                    if (Int64.Parse(line[0]) == 0) IsWeighted = false; else IsWeighted = true;
+
+                    if (Int64.Parse(line[1]) == 0) IsOriented = false; else IsOriented = true;
+
+                    _isweighted = IsWeighted;
+                    for (int i = 2; i < line.Length; i++)
+                    {
+                        string[] currentLine = line[i].Split(' ');
+                        V_list = new List<Dictionary<Vertex, Weight>>();
+                        int[] a;
+                        if (!IsWeighted)
+                        {
+                            a = line[i].Split(' ', ':').
+                                          Where(x => !string.IsNullOrWhiteSpace(x)).
+                                          Select(x => int.Parse(x)).ToArray();
+                            tmp_value = new Dictionary<Vertex, Weight>();
+                            for (int j = 1; j < a.Length; j++)
+                            {
+                                x = new Vertex(a[j]);
+                                tmp_value.Add(x, null);
+                                
+                            }
+                            V_list.Add(tmp_value);
+                        }
+                        else
+                        {
+
+                            a = line[i].Split(' ', ':', '(', ')', ',').
+                                       Where(x => !string.IsNullOrWhiteSpace(x)).
+                                       Select(x => int.Parse(x)).ToArray();
+
+                            tmp_value = new Dictionary<Vertex, Weight>();
+                            for (int j = 1; j < a.Length; j++)
+                            {
+                                x = new Vertex(a[j]);
+                                w = new Weight(a[++j]);
+                                tmp_value.Add(x, w);
+                            }
+                            V_list.Add(tmp_value);
+
+                        }
+                        G.Add(new Vertex(a[0]), V_list);
+                    }
+
+                }         
+            }
+            GetEdges();
+            //GetVertex();
+            WriteToCopyFile();
+        }
+
+        #endregion
+
+        #region Methods 
+
+        private void GetEdges() {
+
+            foreach (var key in G.Keys)
+            {
+                foreach (var list_values in G[key])
+                {
+                    foreach (var value in list_values) {
+                        Edges.Add( new Edge(key, value.Key, value.Value));
+                    }
+                }
+            }
+        }
+
+
+        public void AddVertex(Vertex v) {
+            fl = true;
+            foreach (var key in G.Keys)
+                if (key.Equals(v))
+                {
+                    fl = false;
+                    break;
+                }
+            if (fl)
+            {
+                V_list = new List<Dictionary<Vertex, Weight>>();
+                G.Add(v, V_list);
+            }
+            //GetVertex();
+        }
+
+        public void RemoveVertex(Vertex v) {
+            foreach (var key in G.Keys)
+            {
+                if (key.Equals(v))
+                {
+                    G.Remove(key);
+                }
+                else
+                {
+                    V_list = new List<Dictionary<Vertex, Weight>>(G[key]);
+                    foreach (var list_values in V_list)
+                        foreach (var value in list_values)
+                            if (value.Key.Equals(v))
+                                list_values.Remove(value.Key);
+                    
+                }
+            }
+            
+            
+
+        }
+
+
+        private bool EdgeCheck(Edge edge) {
+            foreach (var i in Edges) {
+                if (edge.X.Equals(i.X) && edge.Y.Equals(i.Y))
+                    return false;
+            }
+            return true;
+        
+        }
+        public void AddEdges(Edge edge)
+        {
+            if (exist(edge.X, edge.Y) && EdgeCheck(edge))
+            {
+                if (IsOriented)
+                {
+                    foreach (var key in G.Keys)
+                    {
+                        if (edge.X.Equals(key))
+                        {
+                            tmp_value = new Dictionary<Vertex, Weight>();
+                            if (IsWeighted)
+                                tmp_value.Add(edge.Y, edge.W);
+                            else
+                                tmp_value.Add(edge.Y, null);
+                            G[key].Add(tmp_value);
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var key in G.Keys)
+                    {
+                        if (edge.X.Equals(key))
+                        {
+                            tmp_value = new Dictionary<Vertex, Weight>();
+                            if (IsWeighted)
+                            {
+                                tmp_value.Add(edge.Y, edge.W);
+                                //tmp_value.Add(edge.X, edge.W);
+                            }
+                            else
+                            {
+                                tmp_value.Add(edge.Y, null);
+                                //tmp_value.Add(edge.X, null);
+
+                            }
+                            G[key].Add(tmp_value);
+                        }
+
+                        if (edge.Y.Equals(key))
+                        {
+                            tmp_value = new Dictionary<Vertex, Weight>();
+                            if (IsWeighted)
+                            {
+                                tmp_value.Add(edge.X, edge.W);
+                            }
+                            else
+                            {
+                                tmp_value.Add(edge.X, null);
+
+                            }
+                            G[key].Add(tmp_value);
+                        }
+                    }
+                }
+                Edges.Clear();
+                GetEdges();
+            }
+        }
+
+
+
+        public void RemoveEdge(Vertex v1, Vertex v2) {
+            if (exist(v1,v2)) {
+                foreach (var key in G.Keys)
+                {
+                    if (key.X == v1.X)
+                    {
+                        foreach (var list_values in G[key])
+                        {
+                            foreach (var value in list_values.Keys)
+                                if (value.X == v2.X)
+                                    list_values.Remove(value);
+                        }
+                    }
+                    if (!IsOriented)
+                    {
+                        if (key.X == v2.X)
+                        {
+                            foreach (var list_values in G[key])
+                            {
+                                foreach (var value in list_values.Keys)
+                                    if (value.X == v1.X)
+                                        list_values.Remove(value);
+                            }
+                        }
+                    }
+
+                }
+                Edges.Clear();
+                GetEdges();
+            }
+        }
+
+        private bool exist(Vertex v1, Vertex v2)
+        {
+            bool[] fl = new bool[2];
+            int i = 0;
+            foreach (var key in G.Keys)
+                if (v1.X == key.X || v2.X == key.X)
+                    fl[i++] = true;
+
+            return fl[0] && fl[1];
+
+        }
+
+        public void WriteToNewFile() {
+            string path = @"D:\C#\graph\Graph\result.txt";
+            using (FileStream file = new FileStream(path, FileMode.Create))
+            {
+                using (StreamWriter write = new StreamWriter(file))
+                {
+                    write.WriteLine((Convert.ToInt32(IsWeighted)).ToString());
+                    write.WriteLine((Convert.ToInt32(IsOriented)).ToString());
+
+                    foreach (var key in G.Keys)
+                    {
+                        write.Write($"{key.ToString()}: ");
+                        foreach (var list_values in G[key])
+                        {
+                            if (list_values != null)
+                            {
+                                foreach (var value in list_values)
+                                    if (IsWeighted)
+                                        write.Write($"({value.Key.ToString()},{value.Value.ToString()}) ");
+                                    else write.Write($"{value.Key.ToString()} ");
+                            }
+
+                        }
+                        write.WriteLine();
+                    }
+
+                }
+            }
+        }
+
+        public void WriteToCopyFile() {
+            string path = @"D:\C#\graph\Graph\copy.txt";
+            using (FileStream file = new FileStream(path, FileMode.Create)) {
+                using (StreamWriter write = new StreamWriter(file))
+                {
+                    write.WriteLine((Convert.ToInt32(IsWeighted)).ToString());
+                    write.WriteLine((Convert.ToInt32(IsOriented)).ToString());
+
+                    foreach (var key in G.Keys)
+                    {
+                        write.Write($"{key.ToString()}: ");
+                        foreach (var list_values in G[key])
+                        {
+                            if (list_values != null)
+                            {
+                                foreach (var value in list_values)
+                                    if (IsWeighted)
+                                        write.Write($"({value.Key.ToString()},{value.Value.ToString()}) ");
+                                    else write.Write($"{value.Key.ToString()} ");
+                            }
+
+                        }
+                        write.WriteLine();
+                    }
+
+                }
+            }
+        
+        }
+
+        #endregion
+
+
+    }
+
+    class Vertex{
+
+        public int X { get; set; }
+
+        public Vertex() { }
+        ~Vertex() { }
+        public Vertex(int x) {
+            X = x;
+        }
+
+        public override string ToString()
+        {
+            return $"{X}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Vertex);
+        }
+
+        public  bool Equals(Vertex other)
+        {
+            return X == other.X;
+        }
+    }
+
+
+    class Weight {
+        public int W { get; set; }
+
+        public Weight() { }
+        public Weight(int w)
+        {
+            W = w;
+        }
+
+        public override string ToString()
+        {
+            return $"{W}";
+        }
+
+
+    }
+       
+    class Edge
+    {
+        public Vertex X { get; set; } 
+        public Vertex Y { get; set; } 
+        public Weight W { get; set; }
+
+        public Edge(Vertex x, Vertex y, Weight w) {
+            X = x;
+            Y = y;
+            W = w;
+        }
+
+        public override string ToString() {
+            return $"({X}, {Y}) - {W}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Graph;
+
+namespace Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<Vertex, List<Vertex>> G = new Dictionary<Vertex, List<Vertex>>();
+            //List<Vertex> list = new List<Vertex>();
+            Vertex x = new Vertex();
+            Vertex y = new Vertex();
+            Weight w = new Weight();
+            //Graph[] graphs;
+            bool main_fl = true;
+            bool include_fl = true;
+            string[] pathes = { @"D:\C#\graph\Graph\adjacency_list_1.txt"
+                              , @"D:\C#\graph\Graph\adjacency_list_2.txt"
+                              , @"D:\C#\graph\Graph\adjacency_list_3.txt"
+                              , @"D:\C#\graph\Graph\adjacency_list_4.txt" };
+
+            int action;
+            int value;
+
+
+            while (main_fl)
+            {
+                Console.WriteLine("Choose action: \n1 - Make graph\n0 - Exit");
+                action = Convert.ToInt32(Console.ReadLine());
+                switch (action)
+                {
+                    case 1:
+                        Console.WriteLine("Choose path : ");
+
+                        for (int i = 0; i < pathes.Length; i++)
+                            Console.WriteLine($"{i}: {pathes[i]}");
+
+                        int it = Convert.ToInt32(Console.ReadLine());
+                        Graph g = new Graph(pathes[it]);
+
+
+                        while (include_fl)
+                        {
+                            Console.WriteLine("Choose action: \n0 - Add vertex,\n1 - Remove vertex,\n2 - Add edge\n3 - Remove Edge\n4 - Write to result file\n5 - Write to current file\n6 - exit ");
+                            action = Convert.ToInt32(Console.ReadLine());
+                            switch (action)
+                            {
+                                case 0:
+                                    Console.Write("Vertex: ");
+                                    g.AddVertex(new Vertex(Convert.ToInt32(Console.ReadLine())));
+                                    break;
+
+                                case 1:
+                                    Console.Write("Vertex: ");
+                                    g.RemoveVertex(new Vertex(Convert.ToInt32(Console.ReadLine())));
+                                    break;
+                                case 2:
+                                    Console.Write("First vertex:");
+                                    x = new Vertex((Convert.ToInt32(Console.ReadLine())));
+                                    Console.Write("Second vertex:");
+                                    y = new Vertex((Convert.ToInt32(Console.ReadLine())));
+                                    if (g.isweighted)
+                                    {
+                                        w = new Weight((Convert.ToInt32(Console.ReadLine())));
+                                        g.AddEdges(new Edge(x, y, w));
+                                    }
+                                    else g.AddEdges(new Edge(x, y, null));
+
+                                    break;
+                                case 3:
+                                    Console.Write("First vertex: ");
+                                    x = new Vertex((Convert.ToInt32(Console.ReadLine())));
+                                    Console.Write("Second vertex: ");
+                                    y = new Vertex((Convert.ToInt32(Console.ReadLine())));
+                                    g.RemoveEdge(x, y);
+                                    break;
+                                case 4:
+                                    g.WriteToNewFile();
+                                    break;
+                                case 5:
+                                    g.WriteToCopyFile();
+                                    break;
+                                case 6:
+                                    include_fl = false;
+                                    break;
+                                default:
+                                    include_fl = false;
+                                    break;
+
+                            }
+
+                        }
+                        break;
+                    case 0:
+                        main_fl = false;
+                        break;
+
+                    default:
+                        main_fl = false;
+                        break;
+                }
+            }
+              
+        }
+    }
+}
+
